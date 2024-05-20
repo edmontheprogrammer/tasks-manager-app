3d41f5124b17ee29527313b050acf64725bdf168
Edmon Sebit <edmontheprogrammer@gmail.com>
2024-05-20 14:35:43 -0600

    Creating the views

--- backend/todo/views.py
+++ backend/todo/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+from .serializers import TodoSerializer
+from .models import Todo
+
+
+class TodoView(viewsets):
+    serializer_class = TodoSerializer
+    quertset = Todo.objects.all()
