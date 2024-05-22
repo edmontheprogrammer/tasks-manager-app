aaff4173875afb28425adac1a4f77e5dda5df743
Edmon Sebit <edmontheprogrammer@gmail.com>
2024-05-22 08:38:48 -0600

    Implementing the App component: displayCompleted(), renderTabList(), renderItems() and render() method

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,100 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState, Component } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+const tasks = [
+  {
+    id: 2, 
+    title: "Dunning", 
+    description: "Sending dunning letters to clients for uncollected cash.",
+    completed: false 
+  },
+  {
+    id: 3, 
+    title: "Order Release", 
+    description: "Check out customers accounts and release or block orders accordingly",
+    completed: true
+  },
+  {
+    id: 4, 
+    title: "Weekly Reports", 
+    description: "Sending the weekly reports for overdue invoices",
+    completed: false 
+  },
+]
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      viewCompleted: false, 
+      taskList: tasks 
+    }
+  }
+
+  displayCompleted = status => {
+    if (status) {
+      return this.setstatus({ viewCompleted: true });
+    }
+    return this.setstatus({ viewCompleted: false }); 
+  }
+
+  renderTabList = () => {
+    return (
+      <div className='my-5 tab-list'>
+        <span
+          onClick={() => this.displayCompleted(true)}
+          className={this.state.viewCompleted ? "active" : ""}
+        >
+          Completed
+        </span>
+        <span
+          onClick={() => this.displayCompleted(false)}
+          className={this.state.viewCompleted ? "" : "active"}
+        >
+          Incompleted
+        </span>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    )
+  }
+
+  // Rendering items in the list (completed  || Incompleted )
+  renderItems = () => {
+    const {viewCompleted} = this.state;
+    const newItems = this.state.taskList.filter(
+      item => item.completed == viewCompleted
+    ); 
+  }; 
+
+
+  render() {
+    return (
+      <main className='context'>
+        <h1 className='text-black text-uppercase text-center my-4'>Task Manager</h1>
+        <div className='row'>
+          <div className='col-md-6 col-sma-10 mx-auto p-0'>
+            <div className='card p-3'>
+              <div>
+                <button className='btn btn-warning'>Add Task</button> 
+              </div>
+                {this.renderTabList()}
+                <ul className='list-group list-group-flush'>
+                  {this.renderItems()}
+                </ul>
+            </div>
+          </div>
+        </div>
+
+
+
+      </main>
+    )
+  }
+
+
 }
 
-export default App
+
+
+
+export default App;
